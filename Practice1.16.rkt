8ff4f348a3b55328eb210614767951fad94bc09b
cuipengfei <cuipengfei2008@gmail.com>
2012-10-06 10:04:28 +0800

    added slow-expt

--- Practice1.16.rkt
+++ Practice1.16.rkt
@@ -1,4 +1,5 @@
 #lang racket
+;迭代实现，反复求平方
 (define (fast-expt base n)
   (fast-expt-iter base n 1))
 
@@ -6,3 +7,8 @@
   (cond ((= counter 0) a)
         ((odd? counter) (fast-expt-iter base (- counter 1) (* base a)))
         ((even? counter) (fast-expt-iter (* base base) (/ counter 2) a))))
+
+;递归实现
+(define (slow-expt base n)
+  (if (= n 0) 1
+      (* base (slow-expt base (- n 1)))))
