aef8f8f560ee8d02eb67a4bca1eeb82954699932
cuipengfei <cuipengfei2008@gmail.com>
2015-03-22 17:35:14 +0800

    refactor done

--- chapter1/1.7.rkt
+++ chapter1/1.7.rkt
@@ -1,31 +1,25 @@
 #lang racket
-(define (sqrt-iter guess x)
-  (if (good-enough? (* guess guess) x)
-      guess
-      (sqrt-iter (improve guess x) x)))
-
 (define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (average x y)
-  (/ (+ x y) 2))
+  (/ (+ guess (/ x guess)) 2))
 
 (define (sqrt x)
-  (sqrt-iter 1.0 x))
+  (iter 1.0 x improve sqr))
 
 ;1.8
-
-(define (cube-root-iter guess x)
-  (if (good-enough? (* guess guess guess) x)
-      guess
-      (cube-root-iter (improve-cube guess x) x)))
 
 (define (improve-cube y x)
   (/ (+ (/ x (* y y)) (* 2 y)) 3))
 
 (define (cube-root x)
-  (cube-root-iter 1.0 x))
+  (iter 1.0 x improve-cube cube))
+
+(define (cube x) (* x x x))
 
 ;common
 (define (good-enough? guessed x)
   (< (/ (abs (- guessed x)) x) 0.001))
+
+(define (iter guess x improvef approach)
+  (if (good-enough? (approach guess) x)
+      guess
+      (iter (improvef guess x) x improvef approach)))
