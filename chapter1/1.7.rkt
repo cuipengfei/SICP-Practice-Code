07040b1d944bbd3adea1c95539c2e354fbd99c53
cuipengfei <cuipengfei2008@gmail.com>
2015-03-22 17:11:27 +0800

    1.8 with dup

--- chapter1/1.7.rkt
+++ chapter1/1.7.rkt
@@ -15,3 +15,19 @@
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+
+
+(define (cube-root-iter guess x)
+  (if (good-enough-cube? guess x)
+      guess
+      (cube-root-iter (improve-cube guess x) x)))
+
+(define (good-enough-cube? guess x)
+  (< (abs (- x (* guess guess guess))) 0.001))
+
+(define (improve-cube y x)
+  (/ (+ (/ x (* y y)) (* 2 y)) 3))
+
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
