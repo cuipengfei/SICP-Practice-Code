713f86af007e3ddbdd43c29fe96456f11ebf48f8
cuipengfei <cuipengfei2008@gmail.com>
2015-03-24 17:10:20 +0800

    pretty

--- chapter1/1.12.rkt
+++ chapter1/1.12.rkt
@@ -1,4 +1,5 @@
 #lang racket
 (define (pascal-triangle row col)
   (if (or (= col 1) (= col row)) 1
-        (+ (pascal-triangle (- row 1) (- col 1)) (pascal-triangle (- row 1) col))))
+      (+ (pascal-triangle (- row 1) (- col 1)) 
+         (pascal-triangle (- row 1) col))))
