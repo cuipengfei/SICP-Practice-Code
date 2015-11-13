d4e46f6e0d26e108b2ba7c2c82aedf7ecbd0d97b
cuipengfei <cuipengfei2008@gmail.com>
2015-11-13 14:24:38 +0800

    2.3

--- chapter2/2.2.rkt
+++ chapter2/2.2.rkt
@@ -31,3 +31,28 @@
                     (point-x (seg-end seg))) 2)
               (/ (+ (point-y (seg-start seg))
                     (point-y (seg-end seg))) 2)))
+
+;(print-point (mid-point (make-segment (make-point 1 2) (make-point 3 4))))
+
+; 2.3
+
+(define (make-rect upperLeft lowerRight)
+  (make-segment upperLeft lowerRight))
+;矩形用对角线表示
+
+(define (area rect)
+  (* (height rect) (width rect)))
+
+(define (perimeter rect)
+  (* 2 (+ (height rect) (width rect))))
+
+(define (height rect)
+  (- (point-y (seg-start rect)) (point-y (seg-end rect))))
+
+(define (width rect)
+  (- (point-x (seg-end rect)) (point-x (seg-start rect))))
+
+;(area (make-rect (make-point 10 10) (make-point 20 0)))
+;面积
+;(perimeter (make-rect (make-point 10 10) (make-point 20 0)))
+;周长
