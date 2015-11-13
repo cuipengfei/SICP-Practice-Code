2888832fdb775004f7bf3240f9f61e4b9ae8a43b
cuipengfei <cuipengfei2008@gmail.com>
2015-11-13 14:59:36 +0800

    2.42.42.42.42.42.42.42.4

---
+++ chapter2/2.4.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(car (cons 1 2))
+;((cons 1 2) (lambda (p q) p))
+;((lambda (m) (m 1 2)) (lambda (p q) p))
+;((lambda (p q) p) 1 2)
+;1
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(cdr (cons 1 2))
